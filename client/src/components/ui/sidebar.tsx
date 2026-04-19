4859d05d4cd5a48c6cfa70f2c2fba3384980ce8f
BayajidAlam <bayajidalam2001@gmail.com>
2026-04-18 23:16:56 -0700

    resolved no unsaved var issue

--- client/src/components/ui/sidebar.tsx
+++ client/src/components/ui/sidebar.tsx
@@ -606,10 +606,15 @@ function SidebarMenuSkeleton({
 }: React.ComponentProps<"div"> & {
   showIcon?: boolean
 }) {
-  // Random width between 50 to 90%.
+  // Create a stable pseudo-random width between 50% and 89% per component instance.
+  const id = React.useId()
   const width = React.useMemo(() => {
-    return `${Math.floor(Math.random() * 40) + 50}%`
-  }, [])
+    let hash = 0
+    for (let i = 0; i < id.length; i += 1) {
+      hash = (hash * 31 + id.charCodeAt(i)) % 40
+    }
+    return `${hash + 50}%`
+  }, [id])
 
   return (
     <div
