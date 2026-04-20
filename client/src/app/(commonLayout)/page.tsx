5882bae56f0e0f4c6ea055477d8cc665de234bde
BayajidAlam <bayajidalam2001@gmail.com>
2026-04-20 03:46:20 -0700

    Updated home page

--- client/src/app/(commonLayout)/page.tsx
+++ client/src/app/(commonLayout)/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-        Common
-      <Button variant="outline">Hello World</Button>
+      <h1>Welcome to PH Healthcare</h1>
+      Go to <Link href={"/login"}>Login</Link>
     </div>
   );
 }
